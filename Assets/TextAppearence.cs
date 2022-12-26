6ef45a79376cc501877f56e7afbc3531762f6a04
FirstSlip <ilya20030023@mail.ru>
2022-12-26 08:40:18 +0500

    Add Trader Location

---
+++ Assets/TextAppearence.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextAppearence : MonoBehaviour
+{
+
+    private Text dialog;
+    private string str;
+    private Image e_button;
+    private Coroutine dialogCor;
+    private PlayerController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        controller.enabled = false;
+        dialog = GetComponentInChildren<Text>();
+        dialog.text = "";
+        str = "Hello, traveller!\n" +
+            "You showed power by defeating this embodiment\nof evil. " +
+            "I have something for you, it will help you in\nthis difficult adventure.";
+        dialogCor = StartCoroutine(AddElementsToText());
+        e_button = GetComponentsInChildren<Image>()[4];
+        e_button.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            StopCoroutine(dialogCor);
+            controller.enabled = true;
+            gameObject.SetActive(false);
+        }
+    }
+
+    private IEnumerator AddElementsToText()
+    {
+        foreach (var symbol in str)
+        {
+            dialog.text += symbol;
+            yield return new WaitForSeconds(0.05f);
+        }
+        DoInEndOfDialog();
+    }
+
+    private void DoInEndOfDialog()
+    {
+        e_button.enabled = true;
+    }
+}
---
+++ Assets/TraderInteraction.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TraderInteraction : MonoBehaviour
+{
+    private GameObject inventory;
+    private GameObject hero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hero = GameObject.FindGameObjectWithTag("Player");
+        inventory = GameObject.Find("Canvas");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 range = hero.transform.position - transform.position;
+        if (range.sqrMagnitude <= 4f)
+        {
+            GetComponentsInChildren<SpriteRenderer>()[1].enabled = true;
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                inventory.GetComponent<Inventory>().AddItem(gameObject.name);
+                Destroy(gameObject);
+            }
+        }
+        else
+            GetComponentsInChildren<SpriteRenderer>()[1].enabled = false;
+    }
+}
